4cb5e491139465259c0c923d3f1911ea514bc141
Ifeanyi Osuji <oindubuisi@gmail.com>
2021-05-15 09:58:03 +0100

    added chapter 3 of Deitel exercises

---
+++ src/africa/semicolon/deitelExercises/tddTest/chapter_3/Date.java
@@ -0,0 +1,6 @@
+package africa.semicolon.deitelExercises.tddTest.chapter_3;
+
+public class Date {
+    public Date(int i, int i1, int i2) {
+    }
+}
---
+++ src/africa/semicolon/deitelExercises/tddTest/chapter_3/HeartRates.java
@@ -0,0 +1,6 @@
+package africa.semicolon.deitelExercises.tddTest.chapter_3;
+
+public class HeartRates {
+    public HeartRates(String jane, String doe, Date date) {
+    }
+}
