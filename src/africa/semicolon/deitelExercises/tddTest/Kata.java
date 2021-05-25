95fea9fb4397a6a417040c86feb84fb87ed0c737
Ifeanyi Osuji <oindubuisi@gmail.com>
2021-05-25 21:49:29 +0100

    implemented sentinel and counter control loop on students' grades

--- src/africa/semicolon/deitelExercises/tddTest/Kata.java
+++ src/africa/semicolon/deitelExercises/tddTest/Kata.java
@@ -18,11 +18,60 @@ public class Kata {
         if (reverse == pal)
             System.out.println("Number is Pallindrum");
     }
+    public static void calculateSumAndAverageOfStudentScores(){
+        //Pseudocode
+        //1.Ask the user to enter the number of Students in the class
+        //2. You will need to add the scores of th students, since we dont have the scores yet, the sum is zero
+        //3. Input the first student score
+        //4. add the score the to the sum;
+        //5.  number of scores input which was initially zero is incremented by one.
+        //6. while the number of scores is less than number of students
+                // repeat repeat 3, 4, 5
+        //7. Since we now have the sum of the score as the final value of sum,
+            // calculate the average by dividing sum of the scores by the number of scores.
+
+                // Solution
+        int sum = 0;
+        System.out.println("How many Students are in your class? ");
+        Scanner input = new Scanner(System.in);
+        int numberOfStudents = input.nextInt();
+        System.out.println("Input the first score ");
+        int score = input.nextInt();
+        int numberOfScores = 1;
+        sum += score;
+        while (numberOfScores < numberOfStudents){
+            System.out.println("Input the "+ (numberOfScores+ 1) +" score");
+            score = input.nextInt();
+            sum+=score;
+            numberOfScores++;
+        }
+        System.out.println("Sum of the scores = "+sum);
+        System.out.println("Average of the scores = "+ (sum * 1.0)/numberOfScores);
+    }
+    public static void calculateAnotherSumAndAverageScores(){
+        int sum = 0;
+        int numberOfScores = 0;
+        Scanner input = new Scanner(System.in);
+        while(true){
+            System.out.println("Enter a score or enter -1 to exit");
+            int score = input.nextInt();
+            if(score == -1)
+                break;
+            sum += score;
+            numberOfScores++;
+        }
+        System.out.println("Sum of the scores = "+sum);
+        System.out.println("Average of the scores = "+sum * 1.0/numberOfScores);
+
+    }
 
 
     public static void main(String[] args) {
-
-        pallindrum();
+//        System.out.println(checkIfANumberIsPrime(3));
+        //pallindrum();
+        //System.out.println(isPrime(4));
+        //calculateSumAndAverageOfStudentScores();
+        calculateAnotherSumAndAverageScores();
     }
 
     public int determinePriceByQuantity(int quantity) {
@@ -66,8 +115,29 @@ public class Kata {
         return count + 2;
     }
 
-    public int getSellersProfit(int numberOfCopies) {
+    public  int getSellersProfit(int numberOfCopies) {
         int sellersPrice = numberOfCopies * 2000;
         return sellersPrice - determinePriceByQuantity(numberOfCopies);
     }
+    public  static boolean checkIfANumberIsPrime(int number){
+        if(number == 1)
+            return true;
+        if(number == 2)
+            return true;
+        if(number % 2 == 0)
+            return false;
+        for(int i =3; i<=Math.sqrt(number); i+=2) {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+
+     }
+
+     public static boolean isPrime(int number){
+        for (int i = 2; i < Math.sqrt(number) + 1; i++){
+            if(number%i == 0) return false;
+        }
+        return true;
+     }
 }
